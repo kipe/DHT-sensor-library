29815d011c7da685024ad8acfababc34c32d3a36
Kimmo Huoman <kipenroskaposti@gmail.com>
2013-07-19 20:46:26 +0300

    small changes

--- DHT.h
+++ DHT.h
@@ -21,19 +21,20 @@ written by Adafruit Industries
 #define AM2301 21
 
 class DHT {
- private:
-  uint8_t data[6];
-  uint8_t _pin, _type, _count;
-  boolean read(void);
-  unsigned long _lastreadtime;
-  boolean firstreading;
+    private:
+        uint8_t data[6];
+        uint8_t _pin, _type, _count;
+        boolean read(void);
+        unsigned long _lastreadtime;
+        boolean firstreading;
 
- public:
-  DHT(uint8_t pin, uint8_t type, uint8_t count=6);
-  void begin(void);
-  float readTemperature(bool S=false);
-  float convertCtoF(float);
-  float readHumidity(void);
-
+    public:
+        DHT(uint8_t pin, uint8_t type, uint8_t count=6);
+        void begin(void);
+        float readTemperature(bool S=false);
+        float convertCtoF(float);
+        float readHumidity(void);
+        float temperature;
+        float humidity;
 };
 #endif
